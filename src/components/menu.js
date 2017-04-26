2cb48c7344c5c770dff7700f19e7ad444efa1978
chaithep <sound014@gmail.com>
2017-04-26 16:50:04 +0700

    Add route

---
+++ src/components/menu.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Header extends Component {
+    renderLinks() {
+        // Show a link to sign in or sign up
+        return [
+            < li className='nav-item' key='signin' >
+                <Link className='nav-link' to='/Shop'>Shop</Link>
+            </li >,
+            <li className='nav-item' key='signout'>
+                <Link className='nav-link' to='/Sale'>Sale</Link>
+            </li>
+        ];
+    }
+
+    render() {
+        return (
+            <nav className='navbar navbar-light'>
+                <Link to='/' className='navbar-brand'>Horeca</Link>
+                <ul className='nav navbar-nav'>
+                    {this.renderLinks()}
+                </ul>
+            </nav>
+        );
+    }
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import App from './components/app';
+import Menu from './components/menu';
+import Shop from './components/shop'
+import Sale from './components/sale'
+
+
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory}>
+      <Route >
+        <Route path='/menu' component={Menu} />
+        <Route path='/Shop' component={Shop} />
+        <Route path='/Sale' component={Sale} />
+      </Route>
+    </Router>
   </Provider>
   , document.querySelector('.container'));
